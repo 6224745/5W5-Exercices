f33f2e346c00a876ab8badeaa7565675d7f7af26
Gerard Ballestero Vidal <6224745@cegepmontpetit.ca>
2026-02-13 11:52:30 -0500

    Labo4

---
+++ Labo4/MVCsignalR/labo.signalr.api/Hubs/TaskHub.cs
@@ -0,0 +1,45 @@
+﻿using labo.signalr.api.Data;
+using labo.signalr.api.Models;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace labo.signalr.api.Hubs
+{
+    public class TaskHub : Hub
+    {
+        private readonly ApplicationDbContext _context;
+        public TaskHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            base.OnConnectedAsync();
+            // TODO: Ajouter votre logique
+            await Clients.Caller.SendAsync("GetAll", await _context.UselessTasks.ToListAsync());
+        }
+        public async Task Add(string text)
+        {
+
+            UselessTask uselessTask = new UselessTask()
+            {
+                Completed = false,
+                Text = text
+            };
+            _context.UselessTasks.Add(uselessTask);
+            await _context.SaveChangesAsync();
+            await Clients.All.SendAsync("GetAll", await _context.UselessTasks.ToListAsync());
+        }
+        public async Task Complete(int id)
+        {
+            UselessTask? task = await _context.FindAsync<UselessTask>(id);
+            if (task != null)
+            {
+                task.Completed = true;
+                await _context.SaveChangesAsync();
+            }
+            await Clients.All.SendAsync("GetAll", await _context.UselessTasks.ToListAsync());
+        }
+    }
+}
---
+++ Labo4/ngPollingEtSignalR/src/app/polling/polling.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { UselessTask } from '../models/UselessTask';
+import { HttpClient } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-polling',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
+  templateUrl: './polling.component.html',
+  styleUrls: ['./polling.component.css'],
+})
+export class PollingComponent implements OnInit {
+  apiUrl = 'https://localhost:7289/api/UselessTasks/';
+  title = 'labo.signalr.ng';
+  tasks: UselessTask[] = [];
+  taskname: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.updateTasks();
+  }
+
+  async complete(id: number) {
+    // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
+    let x = await lastValueFrom(this.http.get<any>(this.apiUrl + "Complete/" + id));
+    console.log(x);
+  }
+
+  async addtask() {
+    // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
+    let x = await lastValueFrom(this.http.post<any>(this.apiUrl + "Add?taskText="+ this.taskname, null));
+    this.tasks.push(x)
+    console.log(x);
+  }
+
+  async updateTasks() {
+    // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
+    let x = await lastValueFrom(this.http.get<any>(this.apiUrl + "GetAll"));
+    this.tasks = x;
+    // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
+    //setTimeout(() => {this.updateTasks()}, 500);
+  }
+}
