d932f2db7b48074213873c8464952dbe606d3480
Gerard Ballestero Vidal <6224745@cegepmontpetit.ca>
2026-04-29 16:27:49 -0400

    Background Service labu 1 finis

---
+++ BackgrounService/BackgroundService-main/BackgroundService/Hubs/GameHub.cs
@@ -0,0 +1,54 @@
+﻿using BackgroundService.Data;
+using BackgroundService.DTOs;
+using BackgroundService.Models;
+using BackgroundService.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BackgroundService.Hubs
+{
+    [Authorize]
+    public class GameHub : Hub
+    {
+        private Game _game;
+        private BackgroundServiceContext _backgroundServiceContext;
+
+        public GameHub(Game game, BackgroundServiceContext backgroundServiceContext)
+        {
+            _game = game;
+            _backgroundServiceContext = backgroundServiceContext;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            _game.AddUser(Context.UserIdentifier!);
+
+            Player player = _backgroundServiceContext.Player.Where(p => p.UserId == Context.UserIdentifier!).Single();
+
+            await Clients.Caller.SendAsync("GameInfo", new GameInfoDTO()
+            {
+                // TODO: Remplir l'information avec les 2 nouveaux features (nbWins et multiplierCost)
+                NbWins = player.NbWins,
+                MultiplierCost = 10
+            });
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            _game.RemoveUser(Context.UserIdentifier!);
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public void Increment()
+        {
+            _game.Increment(Context.UserIdentifier!);
+        }
+
+        // Ajouter une méthode pour pouvoir acheter un multiplier
+        public void BuyMultiplier()
+        {
+            _game.BuyMultiplier(Context.UserIdentifier!);
+        }
+    }
+}
---
+++ BackgrounService/BackgroundService-main/BackgroundService/Services/Game.cs
@@ -0,0 +1,137 @@
+﻿using BackgroundService.Data;
+using BackgroundService.Hubs;
+using BackgroundService.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using SuperChance.DTOs;
+
+namespace BackgroundService.Services
+{
+    public class UserData
+    {
+        public int Score { get; set; } = 0;
+        // TODO: Ajouter une propriété pour le multiplier
+        public int Multiplier { get; set; } = 1;
+    }
+
+    public class Game : Microsoft.Extensions.Hosting.BackgroundService
+    {
+        public const int DELAY = 30 * 1000;
+        public const int MULTIPLIER_BASE_PRICE = 10;
+
+        private Dictionary<string, UserData> _data = new();
+
+        private IHubContext<GameHub> _gameHub;
+
+        private IServiceScopeFactory _serviceScopeFactory;
+
+        public Game(IHubContext<GameHub> gameHub, IServiceScopeFactory serviceScopeFactory)
+        {
+            _gameHub = gameHub;
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+
+        public void AddUser(string userId)
+        {
+            _data[userId] = new UserData();
+        }
+
+        public void RemoveUser(string userId)
+        {
+            _data.Remove(userId);
+        }
+
+        public void Increment(string userId)
+        {
+            UserData userData = _data[userId];
+            // TODO: Ajouter la valeur du muliplier au lieu d'ajouter 1
+            userData.Score += userData.Multiplier;
+        }
+
+        // TODO: Ajouter une méthode pour acheter un multiplier. Le coût est le prix de base * le multiplier actuel
+        // Les prix sont donc de 10, 20, 40, 80, 160 (Si le prix de base est 10)
+        // Réduire le score du coût du multiplier
+        // Doubler le multiplier du joueur
+
+        public void BuyMultiplier(string userId)
+        {
+            UserData userData = _data[userId];
+            userData.Score -= userData.Multiplier * MULTIPLIER_BASE_PRICE;
+            userData.Multiplier *= 2;
+        }
+
+        public async Task EndRound(CancellationToken stoppingToken)
+        {
+            List<string> winners = new List<string>();
+            int biggestValue = 0;
+            // Reset des compteurs
+            foreach (var key in _data.Keys)
+            {
+                int value = _data[key].Score;
+                if (value > 0 && value >= biggestValue)
+                {
+                    if (value > biggestValue)
+                    {
+                        winners.Clear();
+                        biggestValue = value;
+                    }
+                    winners.Add(key);
+                }
+            }
+
+            // Reset
+            foreach (var key in _data.Keys)
+            {
+                // TODO: On remet le multiplier à 1!
+                _data[key].Score = 1;
+            }
+
+            // Aucune participation!
+            if (biggestValue == 0)
+            {
+                RoundResult noResult = new RoundResult()
+                {
+                    Winners = null,
+                    NbClicks = 0
+                };
+                await _gameHub.Clients.All.SendAsync("EndRound", noResult, stoppingToken);
+                return;
+            }
+
+            using (IServiceScope scope = _serviceScopeFactory.CreateScope())
+            {
+                BackgroundServiceContext backgroundServiceContext =
+                    scope.ServiceProvider.GetRequiredService<BackgroundServiceContext>();
+
+                // TODO: Mettre à jour et sauvegarder le nbWinds des joueurs
+                var pWiners = await backgroundServiceContext.Player.Where(p => winners.Contains(p.UserId)).ToListAsync();
+                foreach(var player in pWiners)
+                {
+                    player.NbWins++;
+                }
+
+                await backgroundServiceContext.SaveChangesAsync();
+
+                List<IdentityUser> users = await backgroundServiceContext.Users.Where(u => winners.Contains(u.Id)).ToListAsync();
+
+                RoundResult roundResult = new RoundResult()
+                {
+                    Winners = users.Select(p => p.UserName)!,
+                    NbClicks = biggestValue,
+                };
+                await _gameHub.Clients.All.SendAsync("EndRound", roundResult, stoppingToken);
+            }
+        }
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(DELAY, stoppingToken);
+                await EndRound(stoppingToken);
+            }
+        }
+    }
+}
